c6bd60e96c1bea634f3f9bcbe35bbc3f4220882c
ZipCPU <dgisselq@ieee.org>
2021-09-04 19:39:08 -0400

    Style updates to TMDS components

--- rtl/hdmi/tmdsdecode.v
+++ rtl/hdmi/tmdsdecode.v
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 //
 // Filename: 	tmdsdecode.v
-//
+// {{{
 // Project:	vgasim, a Verilator based VGA simulator demonstration
 //
 // Purpose:	Convert incoming TMDS data into usable pixel and packet data.
@@ -10,9 +10,9 @@
 //		Gisselquist Technology, LLC
 //
 ////////////////////////////////////////////////////////////////////////////////
-//
-// Copyright (C) 2015-2020, Gisselquist Technology, LLC
-//
+// }}}
+// Copyright (C) 2015-2021, Gisselquist Technology, LLC
+// {{{
 // This program is free software (firmware): you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or (at
@@ -27,62 +27,73 @@
 // with this program.  (It's in the $(ROOT)/doc directory.  Run make with no
 // target there if the PDF file isn't present.)  If not, see
 // <http://www.gnu.org/licenses/> for a copy.
-//
+// }}}
 // License:	GPL, v3, as defined and found on www.gnu.org,
+// {{{
 //		http://www.gnu.org/licenses/gpl.html
-//
 //
 ////////////////////////////////////////////////////////////////////////////////
 //
-//
 `default_nettype	none
-//
-module	tmdsdecode(i_clk, i_word, o_ctl, o_aux, o_pix);
-	input	wire		i_clk;
-	input	wire	[9:0]	i_word;
-	output	wire	[1:0]	o_ctl;
-	output	wire	[6:0]	o_aux;
-	output	wire	[7:0]	o_pix;
+// }}}
+module	tmdsdecode(
+		// {{{
+		input	wire		i_clk,
+		input	wire	[9:0]	i_word,
+		output	wire	[1:0]	o_ctl,
+		output	wire	[6:0]	o_aux,
+		output	wire	[7:0]	o_pix
+		// }}}
+	);
 
+	// Local declarations
+	// {{{
+	reg	[1:0]	r_ctl;
+	reg	[6:0]	r_aux;
 	reg	[7:0]	r_pix;
 	wire	[9:0]	first_midp;
 	wire	[9:0]	brev_word;
-	reg	[6:0]	r_aux;
-	reg	[1:0]	r_ctl;
-	genvar		k;
+	// }}}
 
+	// r_pix generation
+	// {{{
 	assign	first_midp = {
 			((i_word[0]) ? (~i_word[9:2]) : (i_word[9:2])),
 			i_word[1:0] };
 
 	always @(posedge i_clk)
+	if (first_midp[1])
 	begin
-		if (first_midp[1])
-		begin
-			r_pix[0] <= (first_midp[9]);
-			r_pix[1] <= (first_midp[8] ^ first_midp[9]);
-			r_pix[2] <= (first_midp[7] ^ first_midp[8]);
-			r_pix[3] <= (first_midp[6] ^ first_midp[7]);
-			r_pix[4] <= (first_midp[5] ^ first_midp[6]);
-			r_pix[5] <= (first_midp[4] ^ first_midp[5]);
-			r_pix[6] <= (first_midp[3] ^ first_midp[4]);
-			r_pix[7] <= (first_midp[2] ^ first_midp[3]);
-		end else begin
-			r_pix[0] <=  first_midp[9];
-			r_pix[1] <= !(first_midp[8] ^ first_midp[9]);
-			r_pix[2] <= !(first_midp[7] ^ first_midp[8]);
-			r_pix[3] <= !(first_midp[6] ^ first_midp[7]);
-			r_pix[4] <= !(first_midp[5] ^ first_midp[6]);
-			r_pix[5] <= !(first_midp[4] ^ first_midp[5]);
-			r_pix[6] <= !(first_midp[3] ^ first_midp[4]);
-			r_pix[7] <= !(first_midp[2] ^ first_midp[3]);
-		end
+		r_pix[0] <= (first_midp[9]);
+		r_pix[1] <= (first_midp[8] ^ first_midp[9]);
+		r_pix[2] <= (first_midp[7] ^ first_midp[8]);
+		r_pix[3] <= (first_midp[6] ^ first_midp[7]);
+		r_pix[4] <= (first_midp[5] ^ first_midp[6]);
+		r_pix[5] <= (first_midp[4] ^ first_midp[5]);
+		r_pix[6] <= (first_midp[3] ^ first_midp[4]);
+		r_pix[7] <= (first_midp[2] ^ first_midp[3]);
+	end else begin
+		r_pix[0] <=  first_midp[9];
+		r_pix[1] <= !(first_midp[8] ^ first_midp[9]);
+		r_pix[2] <= !(first_midp[7] ^ first_midp[8]);
+		r_pix[3] <= !(first_midp[6] ^ first_midp[7]);
+		r_pix[4] <= !(first_midp[5] ^ first_midp[6]);
+		r_pix[5] <= !(first_midp[4] ^ first_midp[5]);
+		r_pix[6] <= !(first_midp[3] ^ first_midp[4]);
+		r_pix[7] <= !(first_midp[2] ^ first_midp[3]);
 	end
+	// }}}
 
+	// Bit-reverse i_word
+	// {{{
+	genvar	k;
 	generate for(k=0; k<10; k=k+1)
 		assign brev_word[k] = i_word[9-k];
 	endgenerate
+	// }}}
 
+	// AUX and control channel decoding
+	// {{{
 	always @(posedge i_clk)
 	begin
 		r_aux <= 7'h0;
@@ -119,14 +130,17 @@ module	tmdsdecode(i_clk, i_word, o_ctl, o_aux, o_pix);
 		default: begin end
 		endcase
 	end
+	// }}}
 
 	assign	o_ctl  = r_ctl;
 	assign	o_aux  = r_aux;
 	assign	o_pix  = r_pix;
 
 	// Make verilator happy
+	// {{{
 	// verilator lint_off UNUSED
 	wire	unused;
 	assign	unused = first_midp[0];
 	// verilator lint_on  UNUSED
+	// }}}
 endmodule
